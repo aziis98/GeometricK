13b369a077a6b6962d17703eaceeb326cbd819ae
aziis98 <antonio.delucreziis@gmail.com>
2016-06-14 19:57:29 +0200

    Added the Window

--- src/com/aziis98/geometric/main.kt
+++ src/com/aziis98/geometric/main.kt
@@ -1,20 +1,31 @@
 package com.aziis98.geometric
 
+import com.aziis98.geometric.window.*
 import com.aziis98.geometric.window.Window
+import java.awt.*
 
 // Copyright 2016 Antonio De Lucreziis
 
-fun main(args: Array<String>) {
-
-}
+fun main(args: Array<String>) { Geometric }
 
 object Geometric : Window() {
 
     override fun initWindow() {
-        width = 1000
-        height = 900
+        size = size(1000, 900)
         title = "Geometric"
+        resizeable = true
     }
 
+    override fun paint(g: Graphics2D) {
+        g.background = Color.WHITE
+        g.clearRect(0, 0, width, height)
+
+        g.color = Color.RED
+        g.drawRect(300, 300, (100 + 50 * Math.sin(totalUpdates / 15.0)).toInt(), 100)
+    }
+
+    override fun update() {
+
+    }
 
 }
--- src/com/aziis98/geometric/window/window.kt
+++ src/com/aziis98/geometric/window/window.kt
@@ -1,20 +1,128 @@
 package com.aziis98.geometric.window
 
-import java.awt.Canvas
+import java.awt.*
 import javax.swing.*
+import kotlin.concurrent.thread
 
 // Copyright 2016 Antonio De Lucreziis
 
 abstract class Window : Canvas() {
 
-    val jframe: JFrame = JFrame().apply {
-        defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-        setLocationRelativeTo(null)
+    val jframe: JFrame = JFrame()
+
+    init {
+        initWindow()
+
+        jframe.add(this)
+
+        jframe.defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+        jframe.setLocationRelativeTo(null)
+        jframe.isVisible = true
+
+        thread { applicationLoop() }
     }
 
     // Abstract Methods
 
     abstract fun initWindow()
+    abstract fun paint(g: Graphics2D)
+    abstract fun update()
+
+    // Rendering
+
+    var interpolation = 1.0
+    internal var frameCount = 0
+    var fps = 0
+
+    val timeBetweenUpdates: Int
+        get() = 1000000000 / 30
+
+    val timeBetweenRenders: Int
+        get () = 1000000000 / 60
+
+    internal var lastUpdateTime = System.nanoTime()
+
+    internal var lastRenderTime = System.nanoTime()
+
+    internal var lastSecondTime = (lastUpdateTime / 1000000000).toInt()
+
+    var maxUpdatesBeforeRender = 5
+
+    var showFPS = true
+
+    var freshBuffer = false
+
+    var totalUpdates = 0
+
+    fun applicationLoop() {
+        while (jframe.isVisible) {
+
+            var now = System.nanoTime()
+            var updateCount = 0
+
+            while (now - lastUpdateTime > timeBetweenUpdates && updateCount < maxUpdatesBeforeRender) {
+                update()
+                lastUpdateTime += timeBetweenUpdates
+                updateCount++
+
+                totalUpdates++
+            }
+
+            //If for some reason an update takes forever, we don't want to do an insane number of catchups.
+            //If you were doing some sort of game that needed to keep EXACT time, you would get rid of this.
+            if (now - lastUpdateTime > timeBetweenUpdates) {
+                lastUpdateTime = now - timeBetweenUpdates
+            }
+
+            //Render. To do so, we need to calculate interpolation for a smooth render.
+            interpolation = Math.min(1.0, ((now - lastUpdateTime) / timeBetweenUpdates).toDouble())
+            render()
+            lastRenderTime = now
+
+            //Update the frames we got.
+            val thisSecond = (lastUpdateTime / 1000000000).toInt()
+
+            if (thisSecond > lastSecondTime) {
+                if (showFPS) System.out.println("$frameCount FPS")
+                fps = frameCount
+                frameCount = 0
+                lastSecondTime = thisSecond
+            }
+
+            //Yield until it has been at least the target time between renders. This saves the CPU from hogging.
+            while (now - lastRenderTime < timeBetweenRenders && now - lastUpdateTime < timeBetweenUpdates) {
+
+                Thread.`yield`()
+
+                //This stops the app from consuming all your CPU. It makes this slightly less accurate, but is worth it.
+                //You can remove this line and it will still work (better), your CPU just climbs on certain OSes.
+                //FYI on some OS's this can cause pretty bad stuttering. Scroll down and have a look at different peoples' solutions to this.
+
+                Thread.sleep(1)
+
+                now = System.nanoTime()
+            }
+
+        }
+    }
+
+    internal fun render() {
+        val bs = bufferStrategy
+        if (bs == null || freshBuffer) {
+            createBufferStrategy(3)
+            freshBuffer = false
+            return
+        }
+
+        val g = bs.drawGraphics as Graphics2D
+
+        paint(g)
+
+        g.dispose()
+        bs.show()
+
+        frameCount++
+    }
 
     // Binding properties
 
